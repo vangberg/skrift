529d2891af73c3ce9809eaf87c752085b8581c5b
harry <harry@penguin>
2019-12-30 21:02:55 +0100

    Serialize Slate to MD

---
+++ src/editor/serialize.ts
@@ -0,0 +1,50 @@
+import { Node, Text } from 'slate'
+
+export function serialize(nodes: Node[]): string {
+  let out = ""
+  
+  while (nodes.length > 0) {
+    const next = nodes.shift() !
+    out += serializeNode(next)
+  }
+
+  return out.trim()
+}
+
+function serializeNode(node: Node): string {
+  if (Text.isText(node)) {
+    return node.text
+  }
+
+  switch (node.type) {
+    case 'heading1':
+      return heading1(node)
+      break
+    case 'paragraph':
+      return paragraph(node)
+      break
+    case 'note-link':
+      return noteLink(node)
+      break
+  }
+
+  return ""
+}
+
+function serializeChildren(node: Node): string {
+  return node.children.map(serializeNode).join('')
+}
+
+function heading1(node: Node): string {
+  return '# ' + serializeChildren(node) + '\n'
+}
+
+function paragraph(node: Node): string {
+  return '\n' + serializeChildren(node) + '\n'
+}
+
+function noteLink(node: Node): string {
+  return serializeChildren(node)
+}
+
+export default {}
