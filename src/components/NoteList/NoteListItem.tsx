5c783a240649b8108ea382bb4884e80c94ec39de
Harry Vangberg <harry@vangberg.name>
2020-01-06 09:51:15 +0100

    Remove unused imports

--- src/components/NoteList/NoteListItem.tsx
+++ src/components/NoteList/NoteListItem.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState, useMemo } from 'react'
+import React, { useContext, useMemo } from 'react'
 import { StoreContext } from '../../store'
 
 type Props = {
