fd443cb5673a52c41decc9f486c80b72b965f666
harry <harry@penguin>
2020-02-03 20:30:43 +0100

    Firebase WIP

--- src/containers/SignupContainer.tsx
+++ src/containers/SignupContainer.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
+import * as firebase from 'firebase/app'
+
+const uiConfig = {
+  signInFlow: 'popup',
+  signInSuccessUrl: '/signedIn',
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+  ]
+};
 
 export const SignupContainer: React.FC = () => {
   return (
-    <h1>Signup</h1>
+    <div>
+      <h1>Signup</h1>
+
+      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+    </div>
   );
 };
