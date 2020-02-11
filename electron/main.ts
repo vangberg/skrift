2305d746e699f51730b456d40a252bf2309153ad
Harry Vangberg <harry@vangberg.name>
2020-02-11 11:23:01 +0100

    OK ELECTRON!

---
+++ electron/main.ts
@@ -0,0 +1,39 @@
+import { app, BrowserWindow } from "electron";
+import * as path from "path";
+import * as url from "url";
+
+let mainWindow: Electron.BrowserWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600
+  });
+
+  const startUrl =
+    process.env.ELECTRON_START_URL ||
+    url.format({
+      pathname: path.join(__dirname, "/../build/index.html"),
+      protocol: "file:",
+      slashes: true
+    });
+  mainWindow.loadURL(startUrl);
+  mainWindow.webContents.openDevTools();
+  mainWindow.on("closed", () => {
+    mainWindow.destroy();
+  });
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
