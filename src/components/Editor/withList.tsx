4e43822dfda35ec6f02ee9b69ea89c97ea7407a1
Harry Vangberg <harry@vangberg.name>
2020-03-25 13:02:01 +0100

    Allow linebreaks again :)

--- src/components/Editor/withList.tsx
+++ src/components/Editor/withList.tsx
@@ -1,5 +1,28 @@
 import { Editor, Node, Transforms, Descendant, Element } from "slate";
 
+const handleInsertBreak = (editor: Editor): boolean => {
+  const block = Editor.above(editor, {
+    match: n => Editor.isBlock(editor, n)
+  });
+
+  if (!block || block[0].type !== "list-item") {
+    return false;
+  }
+
+  if (Editor.isEmpty(editor, block[0])) {
+    Transforms.setNodes(editor, {
+      type: "paragraph"
+    });
+
+    Transforms.unwrapNodes(editor, {
+      match: n => ["bulleted-list", "numbered-list"].includes(n.type),
+      split: true
+    });
+    return true;
+  }
+
+  return false;
+};
 export const withList = (editor: Editor): Editor => {
   const { normalizeNode } = editor;
 
@@ -43,27 +66,7 @@ export const withList = (editor: Editor): Editor => {
   const { insertBreak } = editor;
 
   editor.insertBreak = () => {
-    const block = Editor.above(editor, {
-      match: n => Editor.isBlock(editor, n)
-    });
-
-    if (!block || block[0].type !== "list-item") {
-      return false;
-    }
-
-    if (Editor.isEmpty(editor, block[0])) {
-      Transforms.setNodes(editor, {
-        type: "paragraph"
-      });
-
-      Transforms.unwrapNodes(editor, {
-        match: n => ["bulleted-list", "numbered-list"].includes(n.type),
-        split: true
-      });
-      return;
-    }
-
-    insertBreak();
+    handleInsertBreak(editor) || insertBreak();
   };
 
   return editor;
