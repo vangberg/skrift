3bcce562b4ccd7caf1f732a20dc95acc80350f7e
Harry Vangberg <harry@vangberg.name>
2020-04-16 11:30:03 +0200

    Only show RSS etc when in dev

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import isDev from "electron-is-dev";
 import { WorkspaceContainer } from "./containers/WorkspaceContainer";
 import { DevInfo } from "./components/DevInfo";
 
 export const App: React.FC = () => {
   return (
     <div className="flex flex-1">
-      <DevInfo />
+      {isDev && <DevInfo />}
       <WorkspaceContainer />
     </div>
   );
