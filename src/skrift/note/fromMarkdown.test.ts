32fc2e1fc026e665be0f7f302ce85a0ccac00cbd
Harry Vangberg <harry@vangberg.name>
2021-01-21 20:56:00 +0100

    Parse with MarkdownIt

--- src/skrift/note/fromMarkdown.test.ts
+++ src/skrift/note/fromMarkdown.test.ts
@@ -2,26 +2,25 @@ import { Note } from ".";
 
 const fullNote = `# A title
 
-Some content. [[123]].
-Another link: [[456]]`;
+Some content. [#](123.md).
+Another link: [#](456.md)`;
 
 describe("parseMarkdown", () => {
   describe("with full note", () => {
     const note = Note.fromMarkdown(fullNote);
 
     it("parses title", () => {
-      expect(note.title).toEqual("# A title");
+      expect(note.title).toEqual("A title");
     });
 
     it("parses body", () => {
       expect(note.body).toEqual(
-        "Some content. [[123]].\nAnother link: [[456]]"
+        "Some content. [#](123.md).\nAnother link: [#](456.md)"
       );
     });
 
-    // FIX
     it("parses links", () => {
-      expect(note.linkIds).toEqual(new Set([]));
+      expect(note.linkIds).toEqual(new Set(["123.md", "456.md"]));
     });
   });
 });
--- src/skrift/note/fromMarkdown.ts
+++ src/skrift/note/fromMarkdown.ts
@@ -1,3 +1,6 @@
+import markdownit from "markdown-it";
+import Token from "markdown-it/lib/token";
+
 interface ParsedNote {
   markdown: string;
   title: string;
@@ -5,13 +8,43 @@ interface ParsedNote {
   linkIds: Set<string>;
 }
 
+const parser = markdownit("commonmark");
+
+const getLinks = (tokens: Token[]): Set<string> => {
+  const links = new Set<string>();
+
+  tokens.forEach((token) => {
+    if (token.type !== "inline") return;
+
+    token.children?.forEach((child) => {
+      if (child.type !== "link_open") return;
+
+      const href = child.attrs?.find(([key]) => key === "href");
+
+      if (!href) return;
+
+      links.add(href[1]);
+    });
+  });
+
+  return links;
+};
+
 export function fromMarkdown(markdown: string): ParsedNote {
-  const lines = markdown.split(/\r?\n/);
+  const tokens = parser.parse(markdown, {});
+
+  const inline = tokens.filter((token) => token.type === "inline");
+  const title = inline[0]?.content || "";
+  const body = inline
+    .slice(1)
+    .map((inline) => inline.content)
+    .join("\n");
+  const linkIds = getLinks(tokens);
 
   return {
     markdown,
-    title: lines[0],
-    linkIds: new Set(),
-    body: lines.slice(1, -1).join("\n"),
+    title,
+    linkIds,
+    body,
   };
 }
