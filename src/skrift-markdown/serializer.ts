be5e78e792b0208fe399a9f3bf72da92fced9cf9
Harry Vangberg <harry@vangberg.name>
2025-01-15 16:26:32 +0100

    Basic implementation of remark-prosemirror

--- src/skrift-markdown/serializer.ts
+++ src/skrift-markdown/serializer.ts
@@ -1,3 +1,49 @@
-import { defaultMarkdownSerializer } from "prosemirror-markdown";
+import { unified } from "unified";
+import remarkStringify from "remark-stringify";
+import {
+  fromProseMirror,
+  fromPmNode,
+  fromPmMark,
+} from "@handlewithcare/remark-prosemirror";
+import type { Node } from "prosemirror-model";
 
-export const markdownSerializer = defaultMarkdownSerializer;
+import { schema } from "./schema";
+
+export function proseMirrorToMarkdown(doc: Node) {
+  // Convert to mdast with the fromProseMirror util.
+  // It takes a schema, a set of node handlers, and a
+  // set of mark handlers, each of which converts a
+  // ProseMirror node or mark to an mdast node.
+  const mdast = fromProseMirror(doc, {
+    schema: schema,
+    nodeHandlers: {
+      // Simple nodes can be converted with the fromPmNode
+      // util.
+      paragraph: fromPmNode("paragraph"),
+      heading: fromPmNode("heading"),
+      list_item: fromPmNode("listItem"),
+      // You can set mdast node properties from the
+      // ProseMirror node or its attrs
+      ordered_list: fromPmNode("list", () => ({
+        ordered: true,
+      })),
+      bullet_list: fromPmNode("list", () => ({
+        ordered: false,
+      })),
+    },
+    markHandlers: {
+      // Simple marks can be converted with the fromPmMark
+      // util.
+      em: fromPmMark("emphasis"),
+      strong: fromPmMark("strong"),
+      // Again, mdast node properties can be set from the
+      // ProseMirror mark attrs
+      link: fromPmMark("link", (mark) => ({
+        url: mark.attrs["href"],
+        title: mark.attrs["title"],
+      })),
+    },
+  });
+
+  return unified().use(remarkStringify).stringify(mdast);
+}
